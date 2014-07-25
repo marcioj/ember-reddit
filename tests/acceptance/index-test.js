a8a4213654ab91aa658c2d0ab6f7cd8940d42e76
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-07-25 15:56:04 -0300

    Make index route tests pass

---
+++ tests/acceptance/index-test.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+
+var App;
+
+module('Acceptance: Index', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('visiting /', function() {
+  visit('/');
+
+  andThen(function() {
+    equal(currentPath(), 'index');
+    equal(find('h1').text(), 'Ember reddit');
+  });
+});
